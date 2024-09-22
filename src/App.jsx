a8407b6d080aa7292583128187bbc1ac485b5504
Gefila Zona Pranata <gefilazonapranata@gmail.com>
2024-09-22 22:23:56 +0700

    Restarting a Quiz

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,12 @@ const initialState = {
 	index: 0,
 	answer: null,
 	points: 0,
-	highscore:0
+	highscore: 0,
 };
 
 function App() {
-	const [{ questions, status, index, answer, points, highscore }, dispatch] = useReducer(
-		reducer,
-		initialState
-	);
+	const [{ questions, status, index, answer, points, highscore }, dispatch] =
+		useReducer(reducer, initialState);
 
 	const numQuestions = questions.length;
 	const maxPossiblePoints = questions.reduce(
@@ -61,7 +59,21 @@ function App() {
 			case "nextQuestion":
 				return { ...state, index: state.index + 1, answer: null };
 			case "finish":
-				return { ...state, status: "finished", highscore: state.points > state.highscore ? state.points : state.highscore };
+				return {
+					...state,
+					status: "finished",
+					highscore:
+						state.points > state.highscore
+							? state.points
+							: state.highscore,
+				};
+			case "restart":
+				return {
+					...initialState,
+					questions: state.questions,
+					status: "ready",
+					highscore: state.highscore,
+				};
 			default:
 				throw new Error("Action unknown");
 		}
@@ -102,7 +114,14 @@ function App() {
 					</>
 				)}
 
-				{status === "finished" && <FinishScreen points={points} maxPossiblePoints={maxPossiblePoints} highscore={highscore}/>}
+				{status === "finished" && (
+					<FinishScreen
+						points={points}
+						maxPossiblePoints={maxPossiblePoints}
+						highscore={highscore}
+						dispatch={dispatch}
+					/>
+				)}
 			</Main>
 		</div>
 	);
